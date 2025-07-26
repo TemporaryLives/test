76ed8b7ede3a705fef9f917290998c4de467a076
TemporaryLives <tempaccko1@gmail.com>
2025-07-26 18:08:07 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/63950370172217cc14652992811204de"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/2ffdc4be941a3e8efa70acb790992fae"))()
